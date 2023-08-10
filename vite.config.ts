cc031e04b93b90ecaf183c9f978a09f455dc1cf6
ethan <zhufeng_solo@163.com>
2023-08-10 19:46:22 +0800

    Build code according to environment variables, specifying port and auto-opening of the page.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
-  return {
-    base: "./",
-    define: {
-      "process.env": {
-        NODE_ENV: mode,
+  let config = {};
+  if (mode === 'dev') {
+    config = {
+      base: './',
+      define: {
+        'process.env': {
+          NODE_ENV: mode,
+        },
       },
-    }
-  };
+      server: {
+        port: 3001,
+        open: true,
+      },
+    };
+  } else if (mode === 'prod') {
+    config = {
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'src/index.ts'),
+          name: 'CesiumPlot',
+          fileName: 'CesiumPlot',
+        },
+      },
+    };
+  }
+  return config;
 });
